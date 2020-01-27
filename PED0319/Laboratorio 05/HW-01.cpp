f7935ddc3d923d26b2119860e91b5467bdc85669
Andrea-09 <00366519@uca.edu.sv>
2020-01-26 20:59:57 -0600

    Lab 05 homework

--- PED0319/Laboratorio 05/HW-01.cpp
+++ PED0319/Laboratorio 05/HW-01.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-#include <string>
-#include <stack>
+#include <iostream> //Muestra entrada y salida en consola
+#include <string>  //Permite el uso de strings
+#include <stack>   //Libreria para pilas
 using namespace std;
 
+//Registro
 struct Products{
     string name;
     float price;
@@ -11,9 +12,10 @@ struct Products{
 
 int option = 0;
 
+//Inicio
 int main()
 {
-    stack <Products> merchandise;
+    stack <Products> merchandise;  //se declara una variable llamada merchandise de tipo products en la pila
     Products aux;
     
     do
@@ -26,14 +28,14 @@ int main()
         cout << "Cantidad en bodega: "; cin >> aux.stock;
         cin.ignore();
         
-        merchandise.push(aux);
+        merchandise.push(aux);   //Ingresa los datos en la pila
         
         cout << "Desea agregar mas datos? (1 = si, 0 = no)" << endl;
         cin >> option;
         cin.ignore();
     } while (option != 0);
 
-    aux = merchandise.top();
+    aux = merchandise.top();  //Muestra los datos de la pila pero no los destruye
 
     cout << "Nombre del producto: " << aux.name << endl;
     cout << "Precio: $" << aux.price << endl;
