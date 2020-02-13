829747449694e67e4f8f29191822a409c1a5a101
Andrea-09 <54600387+Andrea-09@users.noreply.github.com>
2020-02-13 10:11:27 -0600

    Update pokemon.cpp

--- PED0319/Soluciones/pokemon.cpp
+++ PED0319/Soluciones/pokemon.cpp
@@ -15,7 +15,33 @@ struct node{
 int main()
 {
     node* Tree = NULL;
+    int option = 0;
     
+    cout << "Bienvenido a su Pokedex, maestro Pokemon!" << endl;
+    do{
+        int option = 0;
+        int auxEnum = 0;
+        cout << "1. Agregar\n";
+        cout << "2. Recorrer en In-Order\n";
+        cout << "3. Recorrer en Post-Order\n";
+        cout << "4. Salir\n";
+        cout << "Ingrese su opcion: ";
+        cin >> option;
+        cin.ignore();
+        
+        switch(option){
+            case 1:
+            cout << "Ingrese el nombre de su Pokemon: \n";
+            getline(cin, name);
+            cout << "Ingrese el tipo (1. fuego, 2.agua, 3. volador, 4. psitico, 5. fantasma: .\n";
+            cin >> auxEnum; cin.ignore(); auxEnum--;
+            
+            insertInTree(&Tree, surname, name, DUI);
+            break;
+            
+            
+        }while(option != 4)
+    }
     
 
     return 0;
@@ -38,6 +64,6 @@ void insertInTree(node** Tree, string name int KantoPokedex, type Types){
     if(!(*Tree))
         *Tree = createLeaf(name, KantoPokedex, Types);
     
-    
-    
-}
+    else{
+        
+    }
