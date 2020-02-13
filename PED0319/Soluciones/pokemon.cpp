f1a696be20f87cee5cb210c4dc8410e9a3fc4aed
Andrea-09 <54600387+Andrea-09@users.noreply.github.com>
2020-02-13 09:59:12 -0600

    Update pokemon.cpp

--- PED0319/Soluciones/pokemon.cpp
+++ PED0319/Soluciones/pokemon.cpp
@@ -22,5 +22,22 @@ int main()
 }
 
 node* createLeaf (string name, int KantoPokedex, type Types){
+    node* leaf = new node;
+    leaf->name = name;
+    leaf->Types = Types;
+    leaf->KantoPokedex = KantoPokedex;
+    leaf->left = NULL;
+    leaf->right = NULL;
+
+    return leaf;
+    
+}
+
+void insertInTree(node** Tree, string name int KantoPokedex, type Types){
+    
+    if(!(*Tree))
+        *Tree = createLeaf(name, KantoPokedex, Types);
+    
+    
     
 }
